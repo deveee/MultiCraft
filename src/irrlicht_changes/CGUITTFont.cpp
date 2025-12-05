696aed71c5033427e837bafe5b2c2fb6cc994363
Deve <deveee@gmail.com>
2025-12-05 22:13:07 +0100

    Apply bidi reordering only when needed

--- src/irrlicht_changes/CGUITTFont.cpp
+++ src/irrlicht_changes/CGUITTFont.cpp
@@ -949,12 +949,12 @@ void CGUITTFont::setFontHinting(const bool enable, const bool enable_auto_hintin
 	reset_images();
 }
 
-void CGUITTFont::draw(const core::stringw& text, const core::rect<s32>& position, video::SColor color, bool hcenter, bool vcenter, const core::rect<s32>* clip)
+void CGUITTFont::draw(const core::stringw& text, const core::rect<s32>& position, video::SColor color, bool hcenter, bool vcenter, const core::rect<s32>* clip, bool use_rtl)
 {
-	draw(EnrichedString(std::wstring(text.c_str()), color), position, hcenter, vcenter, clip);
+	draw(EnrichedString(std::wstring(text.c_str()), color), position, hcenter, vcenter, clip, use_rtl);
 }
 
-void CGUITTFont::draw(const EnrichedString &text, const core::rect<s32>& position, bool hcenter, bool vcenter, const core::rect<s32>* clip)
+void CGUITTFont::draw(const EnrichedString &text, const core::rect<s32>& position, bool hcenter, bool vcenter, const core::rect<s32>* clip, bool use_rtl)
 {
 	const std::vector<video::SColor> &colors = text.getColors();
 
@@ -989,8 +989,9 @@ void CGUITTFont::draw(const EnrichedString &text, const core::rect<s32>& positio
 
 	// Convert to a unicode string.
 	core::ustring utext = text.getString();
-	
-	utext = applyBidiReordering(utext);
+
+	if (use_rtl)
+		utext = applyBidiReordering(utext);
 
 	// Set up our render map.
 	core::map<u32, CGUITTGlyphPage*> Render_Map;
